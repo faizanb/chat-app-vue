72fecf5710570ea0857a64dbe260411e75b3f57b
Faiza N B <faizanb.mec@gmail.com>
2024-07-20 00:01:36 +0530

    Init setup for project

---
+++ src/store.ts
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+
+// define your typings for the store state
+export interface State {
+  count: number
+}
+
+export const store = createStore<State>({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      // mutate state
+      state.count++
+    }
+  }
+})
